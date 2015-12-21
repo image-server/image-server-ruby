f431037d43dc4e81589824364c2ac5ae6bbe0457
Richard Millan <richardiux@gmail.com>
2015-12-21 13:04:49 -0800

    Base gem

---
+++ lib/image/server/ruby.rb
@@ -0,0 +1,9 @@
+require "image/server/ruby/version"
+
+module Image
+  module Server
+    module Ruby
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/image/server/ruby/version.rb
@@ -0,0 +1,7 @@
+module Image
+  module Server
+    module Ruby
+      VERSION = "0.1.0"
+    end
+  end
+end
