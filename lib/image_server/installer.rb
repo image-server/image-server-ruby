57ab08c9315e47c3b37a2f787c4f7cdcb308cf1f
Richard Millan <richardiux@gmail.com>
2015-12-30 18:59:51 -0800

    Fixes to installer
    - bin path comes from configuration
    - rake task requires installer

--- lib/image_server/installer.rb
+++ lib/image_server/installer.rb
@@ -5,45 +5,57 @@ require 'fileutils'
 
 module ImageServer
   class Installer
-    class << self
-      def install
-        return unless valid_platform?
-        return if installed_latest?
+    attr_reader :configuration
 
-        FileUtils.mkdir_p 'bin'
-        File.open('bin/images', 'wb') do |saved_file|
-          open(remote_url, 'rb') do |read_file|
-            saved_file.write(read_file.read)
-          end
+    def self.install(configuration = ImageServer.configuration)
+      new(configuration).install
+    end
+
+    def initialize(configuration = ImageServer.configuration)
+      @configuration = configuration
+    end
+
+    def install
+      return unless valid_platform?
+      return if installed_latest?
+
+      FileUtils.mkdir_p 'bin'
+      File.open(bin_path, 'wb') do |saved_file|
+        open(remote_url, 'rb') do |read_file|
+          saved_file.write(read_file.read)
         end
-        File.chmod(744, 'bin/images')
       end
+      File.chmod(744, bin_path)
+    end
 
-      def valid_platform?
-        %w(darwin linux).include?(platform)
-      end
+    def valid_platform?
+      %w(darwin linux).include?(platform)
+    end
 
-      def platform
-        `uname -s`.strip.downcase
-      end
+    def platform
+      `uname -s`.strip.downcase
+    end
 
-      def current_version
-        '1.15.0'
-      end
+    def current_version
+      '1.15.0'
+    end
 
-      def executable_name
-        "images-#{platform}-#{current_version}"
-      end
+    def executable_name
+      "images-#{platform}-#{current_version}"
+    end
 
-      def remote_url
-        "https://github.com/image-server/image-server/releases/download/v#{current_version}/#{executable_name}"
-      end
+    def remote_url
+      "https://github.com/image-server/image-server/releases/download/v#{current_version}/#{executable_name}"
+    end
 
-      def installed_latest?
-        File.exist?('bin/images') && `bin/image --version`.split(' ').last.chomp == current_version
-      rescue
-        false
-      end
+    def installed_latest?
+      File.exist?(bin_path) && `#{bin_path} --version`.split(' ').last.chomp == current_version
+    rescue
+      false
+    end
+
+    def bin_path
+      configuration.path_to_binary
     end
   end
 end
--- lib/tasks/image_server.rake
+++ lib/tasks/image_server.rake
@@ -1,6 +1,8 @@
 namespace :image_server do
   desc 'Installs the image server if not available'
   task :install do
+    require 'image_server/installer'
+    
     ImageServer::Installer.install
   end
 end
