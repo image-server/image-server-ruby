c2dd720a615ab43bc71d9a4004d182b2b824ccdc
Richard Millan <richardiux@gmail.com>
2016-01-31 14:04:52 -0800

    Gem is now called image_server not image-server

--- lib/image_server/tasks.rb
+++ lib/image_server/tasks.rb
@@ -1,2 +1,2 @@
-spec = Gem::Specification.find_by_name 'image-server'
+spec = Gem::Specification.find_by_name 'image_server'
 load "#{spec.gem_dir}/lib/tasks/image_server.rake"
--- lib/image_server/version.rb
+++ lib/image_server/version.rb
@@ -1,3 +1,3 @@
 module ImageServer
-  VERSION = '0.3.0'
+  VERSION = '0.3.1'
 end
